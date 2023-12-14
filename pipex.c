4611911adb19e3c311686c6637df1bd6e8ffbb6b
Daniel Beres <dberes@c2r6p4.42vienna.com>
2023-12-14 14:29:32 +0100

    thursday afternoon

--- pipex.c
+++ pipex.c
@@ -6,91 +6,121 @@
 /*   By: dberes <dberes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 15:01:58 by dberes            #+#    #+#             */
-/*   Updated: 2023/12/13 15:03:06 by dberes           ###   ########.fr       */
+/*   Updated: 2023/12/14 14:28:53 by dberes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	main(int argc, char **argv)
+char	*get_path(char **env)
 {
-	(void) argv;
-	int	fd[2];
-	int	pid;
-	int	fd_inf;
-	int	fd_outf;
-	char	*args1[] = {"grep", "a1", NULL};
-	char	*args2[] = {"wc", "-w", NULL};
-	if (argc != 3)
+	int	i;
+
+	i = 0;
+	while (env[i] != NULL)
+	{
+		if (ft_strncmp(env[i], "PATH", 4) == 0)
+			return (env[i]);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*get_dir(char *str, char *cmd)
+{
+	char	**dirs;
+	char	*dir;
+	int		i;
+
+	dirs = ft_split(str, 58);
+	i = 0;
+	cmd = ft_strjoin("/", cmd);
+	while (dirs[i] != NULL)
+	{
+		dir = ft_strjoin(dirs[i], cmd);
+		if (access(dir, F_OK) == 0)
+			return (dir);
+		i++;
+	}
+	return (NULL);
+}
+
+void	child_process(char **argv, int fd[2], char *path, char **envp)
+{
+	char	**args1;
+	char	*directory;
+	int		fd_inf;
+
+	args1 = ft_split(argv[2], 32);
+	directory = get_dir(path, args1[0]);
+	close(fd[0]);
+	fd_inf = open("infile", O_RDONLY);
+	if (fd_inf == -1)
+	{
+		perror("open");
+		exit(EXIT_FAILURE);
+	}
+	dup2(fd_inf, STDIN_FILENO);
+	dup2(fd[1], STDOUT_FILENO);
+	close(fd_inf);
+	close(fd[1]);
+	if (execve(directory, args1, envp) == -1)
+	{
+		perror("execve");
+		exit(EXIT_FAILURE);
+	}
+}
+
+void	parent_process(char **argv, int fd[2], char *path, char **envp)
+{
+	int		fd_outf;
+	char	**args2;
+	char	*directory2;
+
+	args2 = (char **)malloc(sizeof(char *)*);
+	args2 = ft_split(argv[3], 32);
+	directory2 = get_dir(path, args2[0]);
+	wait(NULL);
+	close(fd[1]);
+	fd_outf = open("outfile", O_WRONLY | O_CREAT | O_TRUNC, 0666);
+	if (fd_outf == -1)
+	{
+		perror("open");
+		exit(EXIT_FAILURE);
+	}
+	dup2(fd_outf, STDOUT_FILENO);
+	dup2(fd[0], STDIN_FILENO);
+	close(fd[0]);
+	close(fd_outf);
+	if (execve(directory2, args2, envp) == -1)
+	{
+		perror("Could not execve");
+		exit(EXIT_FAILURE);
+	}
+}
+
+int	main(int argc, char **argv, char **env)
+{
+	int		fd[2];
+	int		pid;
+	char	*path;
+
+	if (argc != 5)
 	{
 		ft_printf("Wrong input.");
 		return (1);
 	}
+	path = get_path(env);
 	if (pipe(fd) == -1)
 		return (1);
 	pid = fork();
 	if (pid == -1)
 		return (2);
 	if (pid == 0)
-	{
-		close(fd[0]);
-		fd_inf = open("infile", O_RDONLY);
-		if (fd_inf == -1)
-        {
-            perror("open");
-            exit(EXIT_FAILURE);
-        }
-		dup2(fd_inf, STDIN_FILENO);
-		dup2(fd[1], STDOUT_FILENO);
-		close(fd_inf);
-		close(fd[1]);
-		if (execve("/usr/bin/grep", args1, NULL) == -1)
-		{
-            perror("execve");
-            exit(EXIT_FAILURE);
-        }
-		/*
-		args1 = ft_split(argv[1], 32);
-		ft_printf("%s", args1[0]);*/
-	}
+		child_process(argv, fd, path, env);
 	else
-	{
-		wait(NULL);
-		close(fd[1]);
-		fd_outf = open("outfile", O_WRONLY | O_CREAT | O_TRUNC, 0666);
-		dup2(fd_outf, STDOUT_FILENO);
-		fprintf(stderr, "test3\n");
-		dup2(fd[0], STDIN_FILENO);
-		close(fd[0]);
-		close(fd_outf);
-		
-		/*args2 = ft_split(argv[2], 32);*/
-		if (execve("/usr/bin/wc", args2, NULL) == -1)
-			perror("Could not execve");
-		
-	}
+		parent_process(argv, fd, path, env);
 	close(fd[0]);
 	close(fd[1]);
-	
-	return(0);
-
-	/*
-		waitpid(pid, NULL, 0);
-		infile = open("infile.txt", O_WRONLY);
-		close(fd[0]);
-		write(fd[1], &y, sizeof(int));
-		printf
-		close(fd[1]);
-	}
-	else
-	{
-		close(fd[1]);
-		printf("Type a number:");
-		read(fd[0], &infile, sizeof(int));
-		close(fd[0]);
-	}
-	file2 = dup2(infile, 1);
-	if (id != 0)
-		wait();
-*/
+	return (0);
 }
--- pipex.h
+++ pipex.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dberes <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/14 14:18:20 by dberes            #+#    #+#             */
+/*   Updated: 2023/12/14 14:18:23 by dberes           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PIPEX_H
 # define PIPEX_H
 
@@ -12,5 +24,9 @@
 # include <fcntl.h>
 # include <sys/types.h> 
 # include <sys/wait.h> 
+
+void	child_process(char **argv, int fd[2], char *PATH, char **envp);
+void	parent_process(char **argv, int fd[2], char *PATH, char **envp);
+char	*get_path(char **env);
 
 #endif
